e6489c771453e0e992452d8e99a9c0053ee59eca
greatby <gills_halogen2i@icloud.com>
2025-07-08 11:16:15 +0530

    added text content to hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,7 +2,7 @@
 import { useEffect, useState } from "react";
 
 const HeroZoomOut = () => {
-  const [progress, setProgress] = useState(0);
+  const [progress, setProgress] = useState(0); // 0 to 1
 
   useEffect(() => {
     const handleScroll = () => {
@@ -43,218 +43,21 @@ const HeroZoomOut = () => {
       <div className="h-screen" />
 
       {/* Next content section */}
-      <div className="relative z-20 px-4 md:px-8 py-20 max-w-4xl mx-auto text-center">
-        <h2 className="text-4xl md:text-5xl font-bold mb-6">Welcome to Gold</h2>
-        <p className="text-lg text-gray-600">
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corrupti
-          modi asperiores accusamus tempore nulla dolore eos assumenda at ab,
-          dolorem nisi sapiente corporis facere, id, repudiandae ea
-          exercitationem. Fugiat ipsam beatae quidem sunt nesciunt itaque, a
-          recusandae fuga, quibusdam quas, dolor iste vero iusto minus eveniet
-          in quisquam odio adipisci ipsa perferendis vitae temporibus?
-          Distinctio reprehenderit exercitationem optio nam amet qui possimus
-          voluptatum iusto tempora maxime dolorum earum, deleniti cumque fuga
-          repellat ea quis ducimus laboriosam quod. Quisquam quidem error
-          aperiam? Placeat fugit aspernatur inventore suscipit quisquam. Dolore
-          quas placeat, sint quos vel ipsa alias odit enim, sed tenetur dolores?
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corrupti
-          modi asperiores accusamus tempore nulla dolore eos assumenda at ab,
-          dolorem nisi sapiente corporis facere, id, repudiandae ea
-          exercitationem. Fugiat ipsam beatae quidem sunt nesciunt itaque, a
-          recusandae fuga, quibusdam quas, dolor iste vero iusto minus eveniet
-          in quisquam odio adipisci ipsa perferendis vitae temporibus?
-          Distinctio reprehenderit exercitationem optio nam amet qui possimus
-          voluptatum iusto tempora maxime dolorum earum, deleniti cumque fuga
-          repellat ea quis ducimus laboriosam quod. Quisquam quidem error
-          aperiam? Placeat fugit aspernatur inventore suscipit quisquam. Dolore
-          quas placeat, sint quos vel ipsa alias odit enim, sed tenetur dolores?
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corrupti
-          modi asperiores accusamus tempore nulla dolore eos assumenda at ab,
-          dolorem nisi sapiente corporis facere, id, repudiandae ea
-          exercitationem. Fugiat ipsam beatae quidem sunt nesciunt itaque, a
-          recusandae fuga, quibusdam quas, dolor iste vero iusto minus eveniet
-          in quisquam odio adipisci ipsa perferendis vitae temporibus?
-          Distinctio reprehenderit exercitationem optio nam amet qui possimus
-          voluptatum iusto tempora maxime dolorum earum, deleniti cumque fuga
-          repellat ea quis ducimus laboriosam quod. Quisquam quidem error
-          aperiam? Placeat fugit aspernatur inventore suscipit quisquam. Dolore
-          quas placeat, sint quos vel ipsa alias odit enim, sed tenetur dolores?
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corrupti
-          modi asperiores accusamus tempore nulla dolore eos assumenda at ab,
-          dolorem nisi sapiente corporis facere, id, repudiandae ea
-          exercitationem. Fugiat ipsam beatae quidem sunt nesciunt itaque, a
-          recusandae fuga, quibusdam quas, dolor iste vero iusto minus eveniet
-          in quisquam odio adipisci ipsa perferendis vitae temporibus?
-          Distinctio reprehenderit exercitationem optio nam amet qui possimus
-          voluptatum iusto tempora maxime dolorum earum, deleniti cumque fuga
-          repellat ea quis ducimus laboriosam quod. Quisquam quidem error
-          aperiam? Placeat fugit aspernatur inventore suscipit quisquam. Dolore
-          quas placeat, sint quos vel ipsa alias odit enim, sed tenetur dolores?
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corrupti
-          modi asperiores accusamus tempore nulla dolore eos assumenda at ab,
-          dolorem nisi sapiente corporis facere, id, repudiandae ea
-          exercitationem. Fugiat ipsam beatae quidem sunt nesciunt itaque, a
-          recusandae fuga, quibusdam quas, dolor iste vero iusto minus eveniet
-          in quisquam odio adipisci ipsa perferendis vitae temporibus?
-          Distinctio reprehenderit exercitationem optio nam amet qui possimus
-          voluptatum iusto tempora maxime dolorum earum, deleniti cumque fuga
-          repellat ea quis ducimus laboriosam quod. Quisquam quidem error
-          aperiam? Placeat fugit aspernatur inventore suscipit quisquam. Dolore
-          quas placeat, sint quos vel ipsa alias odit enim, sed tenetur dolores?
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corrupti
-          modi asperiores accusamus tempore nulla dolore eos assumenda at ab,
-          dolorem nisi sapiente corporis facere, id, repudiandae ea
-          exercitationem. Fugiat ipsam beatae quidem sunt nesciunt itaque, a
-          recusandae fuga, quibusdam quas, dolor iste vero iusto minus eveniet
-          in quisquam odio adipisci ipsa perferendis vitae temporibus?
-          Distinctio reprehenderit exercitationem optio nam amet qui possimus
-          voluptatum iusto tempora maxime dolorum earum, deleniti cumque fuga
-          repellat ea quis ducimus laboriosam quod. Quisquam quidem error
-          aperiam? Placeat fugit aspernatur inventore suscipit quisquam. Dolore
-          quas placeat, sint quos vel ipsa alias odit enim, sed tenetur dolores?
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corrupti
-          modi asperiores accusamus tempore nulla dolore eos assumenda at ab,
-          dolorem nisi sapiente corporis facere, id, repudiandae ea
-          exercitationem. Fugiat ipsam beatae quidem sunt nesciunt itaque, a
-          recusandae fuga, quibusdam quas, dolor iste vero iusto minus eveniet
-          in quisquam odio adipisci ipsa perferendis vitae temporibus?
-          Distinctio reprehenderit exercitationem optio nam amet qui possimus
-          voluptatum iusto tempora maxime dolorum earum, deleniti cumque fuga
-          repellat ea quis ducimus laboriosam quod. Quisquam quidem error
-          aperiam? Placeat fugit aspernatur inventore suscipit quisquam. Dolore
-          quas placeat, sint quos vel ipsa alias odit enim, sed tenetur dolores?
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corrupti
-          modi asperiores accusamus tempore nulla dolore eos assumenda at ab,
-          dolorem nisi sapiente corporis facere, id, repudiandae ea
-          exercitationem. Fugiat ipsam beatae quidem sunt nesciunt itaque, a
-          recusandae fuga, quibusdam quas, dolor iste vero iusto minus eveniet
-          in quisquam odio adipisci ipsa perferendis vitae temporibus?
-          Distinctio reprehenderit exercitationem optio nam amet qui possimus
-          voluptatum iusto tempora maxime dolorum earum, deleniti cumque fuga
-          repellat ea quis ducimus laboriosam quod. Quisquam quidem error
-          aperiam? Placeat fugit aspernatur inventore suscipit quisquam. Dolore
-          quas placeat, sint quos vel ipsa alias odit enim, sed tenetur dolores?
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corrupti
-          modi asperiores accusamus tempore nulla dolore eos assumenda at ab,
-          dolorem nisi sapiente corporis facere, id, repudiandae ea
-          exercitationem. Fugiat ipsam beatae quidem sunt nesciunt itaque, a
-          recusandae fuga, quibusdam quas, dolor iste vero iusto minus eveniet
-          in quisquam odio adipisci ipsa perferendis vitae temporibus?
-          Distinctio reprehenderit exercitationem optio nam amet qui possimus
-          voluptatum iusto tempora maxime dolorum earum, deleniti cumque fuga
-          repellat ea quis ducimus laboriosam quod. Quisquam quidem error
-          aperiam? Placeat fugit aspernatur inventore suscipit quisquam. Dolore
-          quas placeat, sint quos vel ipsa alias odit enim, sed tenetur dolores?
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corrupti
-          modi asperiores accusamus tempore nulla dolore eos assumenda at ab,
-          dolorem nisi sapiente corporis facere, id, repudiandae ea
-          exercitationem. Fugiat ipsam beatae quidem sunt nesciunt itaque, a
-          recusandae fuga, quibusdam quas, dolor iste vero iusto minus eveniet
-          in quisquam odio adipisci ipsa perferendis vitae temporibus?
-          Distinctio reprehenderit exercitationem optio nam amet qui possimus
-          voluptatum iusto tempora maxime dolorum earum, deleniti cumque fuga
-          repellat ea quis ducimus laboriosam quod. Quisquam quidem error
-          aperiam? Placeat fugit aspernatur inventore suscipit quisquam. Dolore
-          quas placeat, sint quos vel ipsa alias odit enim, sed tenetur dolores?
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corrupti
-          modi asperiores accusamus tempore nulla dolore eos assumenda at ab,
-          dolorem nisi sapiente corporis facere, id, repudiandae ea
-          exercitationem. Fugiat ipsam beatae quidem sunt nesciunt itaque, a
-          recusandae fuga, quibusdam quas, dolor iste vero iusto minus eveniet
-          in quisquam odio adipisci ipsa perferendis vitae temporibus?
-          Distinctio reprehenderit exercitationem optio nam amet qui possimus
-          voluptatum iusto tempora maxime dolorum earum, deleniti cumque fuga
-          repellat ea quis ducimus laboriosam quod. Quisquam quidem error
-          aperiam? Placeat fugit aspernatur inventore suscipit quisquam. Dolore
-          quas placeat, sint quos vel ipsa alias odit enim, sed tenetur dolores?
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corrupti
-          modi asperiores accusamus tempore nulla dolore eos assumenda at ab,
-          dolorem nisi sapiente corporis facere, id, repudiandae ea
-          exercitationem. Fugiat ipsam beatae quidem sunt nesciunt itaque, a
-          recusandae fuga, quibusdam quas, dolor iste vero iusto minus eveniet
-          in quisquam odio adipisci ipsa perferendis vitae temporibus?
-          Distinctio reprehenderit exercitationem optio nam amet qui possimus
-          voluptatum iusto tempora maxime dolorum earum, deleniti cumque fuga
-          repellat ea quis ducimus laboriosam quod. Quisquam quidem error
-          aperiam? Placeat fugit aspernatur inventore suscipit quisquam. Dolore
-          quas placeat, sint quos vel ipsa alias odit enim, sed tenetur dolores?
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corrupti
-          modi asperiores accusamus tempore nulla dolore eos assumenda at ab,
-          dolorem nisi sapiente corporis facere, id, repudiandae ea
-          exercitationem. Fugiat ipsam beatae quidem sunt nesciunt itaque, a
-          recusandae fuga, quibusdam quas, dolor iste vero iusto minus eveniet
-          in quisquam odio adipisci ipsa perferendis vitae temporibus?
-          Distinctio reprehenderit exercitationem optio nam amet qui possimus
-          voluptatum iusto tempora maxime dolorum earum, deleniti cumque fuga
-          repellat ea quis ducimus laboriosam quod. Quisquam quidem error
-          aperiam? Placeat fugit aspernatur inventore suscipit quisquam. Dolore
-          quas placeat, sint quos vel ipsa alias odit enim, sed tenetur dolores?
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corrupti
-          modi asperiores accusamus tempore nulla dolore eos assumenda at ab,
-          dolorem nisi sapiente corporis facere, id, repudiandae ea
-          exercitationem. Fugiat ipsam beatae quidem sunt nesciunt itaque, a
-          recusandae fuga, quibusdam quas, dolor iste vero iusto minus eveniet
-          in quisquam odio adipisci ipsa perferendis vitae temporibus?
-          Distinctio reprehenderit exercitationem optio nam amet qui possimus
-          voluptatum iusto tempora maxime dolorum earum, deleniti cumque fuga
-          repellat ea quis ducimus laboriosam quod. Quisquam quidem error
-          aperiam? Placeat fugit aspernatur inventore suscipit quisquam. Dolore
-          quas placeat, sint quos vel ipsa alias odit enim, sed tenetur dolores?
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corrupti
-          modi asperiores accusamus tempore nulla dolore eos assumenda at ab,
-          dolorem nisi sapiente corporis facere, id, repudiandae ea
-          exercitationem. Fugiat ipsam beatae quidem sunt nesciunt itaque, a
-          recusandae fuga, quibusdam quas, dolor iste vero iusto minus eveniet
-          in quisquam odio adipisci ipsa perferendis vitae temporibus?
-          Distinctio reprehenderit exercitationem optio nam amet qui possimus
-          voluptatum iusto tempora maxime dolorum earum, deleniti cumque fuga
-          repellat ea quis ducimus laboriosam quod. Quisquam quidem error
-          aperiam? Placeat fugit aspernatur inventore suscipit quisquam. Dolore
-          quas placeat, sint quos vel ipsa alias odit enim, sed tenetur dolores?
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corrupti
-          modi asperiores accusamus tempore nulla dolore eos assumenda at ab,
-          dolorem nisi sapiente corporis facere, id, repudiandae ea
-          exercitationem. Fugiat ipsam beatae quidem sunt nesciunt itaque, a
-          recusandae fuga, quibusdam quas, dolor iste vero iusto minus eveniet
-          in quisquam odio adipisci ipsa perferendis vitae temporibus?
-          Distinctio reprehenderit exercitationem optio nam amet qui possimus
-          voluptatum iusto tempora maxime dolorum earum, deleniti cumque fuga
-          repellat ea quis ducimus laboriosam quod. Quisquam quidem error
-          aperiam? Placeat fugit aspernatur inventore suscipit quisquam. Dolore
-          quas placeat, sint quos vel ipsa alias odit enim, sed tenetur dolores?
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corrupti
-          modi asperiores accusamus tempore nulla dolore eos assumenda at ab,
-          dolorem nisi sapiente corporis facere, id, repudiandae ea
-          exercitationem. Fugiat ipsam beatae quidem sunt nesciunt itaque, a
-          recusandae fuga, quibusdam quas, dolor iste vero iusto minus eveniet
-          in quisquam odio adipisci ipsa perferendis vitae temporibus?
-          Distinctio reprehenderit exercitationem optio nam amet qui possimus
-          voluptatum iusto tempora maxime dolorum earum, deleniti cumque fuga
-          repellat ea quis ducimus laboriosam quod. Quisquam quidem error
-          aperiam? Placeat fugit aspernatur inventore suscipit quisquam. Dolore
-          quas placeat, sint quos vel ipsa alias odit enim, sed tenetur dolores?
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corrupti
-          modi asperiores accusamus tempore nulla dolore eos assumenda at ab,
-          dolorem nisi sapiente corporis facere, id, repudiandae ea
-          exercitationem. Fugiat ipsam beatae quidem sunt nesciunt itaque, a
-          recusandae fuga, quibusdam quas, dolor iste vero iusto minus eveniet
-          in quisquam odio adipisci ipsa perferendis vitae temporibus?
-          Distinctio reprehenderit exercitationem optio nam amet qui possimus
-          voluptatum iusto tempora maxime dolorum earum, deleniti cumque fuga
-          repellat ea quis ducimus laboriosam quod. Quisquam quidem error
-          aperiam? Placeat fugit aspernatur inventore suscipit quisquam. Dolore
-          quas placeat, sint quos vel ipsa alias odit enim, sed tenetur dolores?
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corrupti
-          modi asperiores accusamus tempore nulla dolore eos assumenda at ab,
-          dolorem nisi sapiente corporis facere, id, repudiandae ea
-          exercitationem. Fugiat ipsam beatae quidem sunt nesciunt itaque, a
-          recusandae fuga, quibusdam quas, dolor iste vero iusto minus eveniet
-          in quisquam odio adipisci ipsa perferendis vitae temporibus?
-          Distinctio reprehenderit exercitationem optio nam amet qui possimus
-          voluptatum iusto tempora maxime dolorum earum, deleniti cumque fuga
-          repellat ea quis ducimus laboriosam quod. Quisquam quidem error
-          aperiam? Placeat fugit aspernatur inventore suscipit quisquam. Dolore
-          quas placeat, sint quos vel ipsa alias odit enim, sed tenetur dolores?
+      <div className="sticky h-screen top-0 z-20 px-4 md:px-8 py-20 max-w-4xl mx-auto text-center">
+        <p className="text-[55px] font-semibold text-black">
+          THE EASIEST WAY TO BUY AND SELL GOLD AND SILVER IN INDIA. YOU CAN NOW
+          BUY AND SELL GOLD AND SILVER BARS ANYTIME EASILY ANYTIME. THE PLATFORM
+          YOU ALWAYS WANTED COMING SOON
+        </p>
+         <p className="text-[55px] font-semibold text-[#ffd700]">
+          THE EASIEST WAY TO BUY AND SELL GOLD AND SILVER IN INDIA. YOU CAN NOW
+          BUY AND SELL GOLD AND SILVER BARS ANYTIME EASILY ANYTIME. THE PLATFORM
+          YOU ALWAYS WANTED COMING SOON
+        </p>
+         <p className="text-[55px] font-semibold text-[#c0c0c0]">
+          THE EASIEST WAY TO BUY AND SELL GOLD AND SILVER IN INDIA. YOU CAN NOW
+          BUY AND SELL GOLD AND SILVER BARS ANYTIME EASILY ANYTIME. THE PLATFORM
+          YOU ALWAYS WANTED COMING SOON
         </p>
       </div>
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,14 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+        extend: {
+      fontFamily: {
+        mono: ['"Roboto Mono"', 'monospace'], 
+        sans: ['"Roboto Mono"', 'sans-serif'],
+      },
+    },
+    },
   },
   plugins: [],
 }
